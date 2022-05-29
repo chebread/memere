48ba1f5239e8543d295e05aad4badf064a769190
Cha Haneum <developer_haneum_11@icloud.com>
2022-05-29 23:31:39 +0900

    feat: restructuring

--- src/app.js
+++ src/app.js
@@ -3,19 +3,18 @@ import './app.css';
 import { router } from './components/router.js';
 
 const app = () => {
-  // render specific route
   const render = () => {
     router(window.location.pathname);
     document.addEventListener('DOMContentLoaded', () => {
-      document.querySelectorAll('a').forEach(item =>
-        item.addEventListener('click', e => {
+      document.body.addEventListener('click', e => {
+        if (e.target.nodeName === 'A') {
           const url = e.target.attributes.href.nodeValue;
           if (url.search(/https?:\/\//) === -1) {
             e.preventDefault();
             router(url);
           }
-        })
-      );
+        }
+      });
     });
   };
   render();
